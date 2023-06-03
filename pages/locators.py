945410c624acb30b2d4792bd1211f023db8b690c
silvade <myanova13@gmail.com>
2023-06-03 15:02:23 +0600

    Move method go_to_login_page to BasePage

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,6 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-
 class LoginPageLocators:
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
@@ -17,3 +13,6 @@ class ProductPageLocators:
     MSG_WITH_BASKET_PRICE = (By.CSS_SELECTOR, '.alert-info .alertinner p:first-child')
     PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main h1')
     PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main .price_color')
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
